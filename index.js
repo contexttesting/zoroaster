6c0a706fa10cf130b9142f8dc8d60ff7108db316
Anton <anton@sobesednik.media>
2017-05-28 23:02:46 +0100

    spawn for windows

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ const spawnCommand = require('spawncommand')
 
 const BIN_PATH = path.join(__dirname, 'bin/zoroaster')
 
+function getSpawnOptions(bin, _args) {
+    'use strict'
+    let program
+    let args = Array.isArray(_args) ? _args : []
+    if (!/^win/.test(process.platform)) { // linux
+        program = bin
+    } else { // windows
+        program = process.env.comspec
+        args = [].concat(['/c', 'node', bin], args)
+    }
+    return { program, args }
+}
+
 /**
  * Start zoroaster process, and return a child process with .promise property.
- * Basically, a spawnCommand wrapper around zoroaster binary.
+ * Basically, a spawnCommand wrapper around zoroaster binary. Works on Windows
+ * as well as Linux.
  * @param {string[]} args An array of strings as arguments
  * @param {object} options Options to pass when creating child process
  * @returns {ChildProcess} An instance of a ChildProcess, with `.promise` property,
  * which will be resolved when tests are finished.
  */
 function run(args, options) {
-    const proc = spawnCommand(BIN_PATH, args, options)
+    const spawnOptions = getSpawnOptions(BIN_PATH, args)
+    const proc = spawnCommand(spawnOptions.program, spawnOptions.args, options)
     return proc
 }
 
