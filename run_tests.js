c7df4d0ab1dae926439227fb0b31de831e3b533b
Anton <anton@sobesednik.media>
2018-03-14 22:35:22 +0000

    better run_tests

--- run_tests.js
+++ run_tests.js
@@ -1,12 +1,33 @@
 /**
- * This file is needed to run tests on Windows from package.json.
+ * This file helps to run tests on Windows, as well as chooses to test ES5 for
+ * Node < 8.6.0. Normal ES7 code is tested with Node 8.6+. The default binary
+ * is actual source code, i.e. no transpilation, and es5 binary can be run with
+ * zoroaster-es5
  */
 
 const { fork } = require('child_process')
 const { resolve } = require('path')
+const { nodeLt } = require('noddy')
+const { bin } = require('./package.json')
 
-const zoroaster = resolve(__dirname, 'bin/zoroaster')
-const spec = resolve(__dirname, 'test/spec')
+const { zoroaster, zoroasterEs5 } = bin
+
+const force = process.argv.some(a => a === '--force')
+if (force) {
+  console.log(`Using the force to summon the dead with ${zoroasterEs5}...`) // eslint-disable-line no-console
+} else {
+  console.log(`Thus spoke Zarathustra ${zoroaster}`)
+}
+const useEs5 = nodeLt('v8.6.0') || force
+
+const ZOROASTER = resolve(__dirname, zoroaster)
+const ZOROASTER_ES5 = resolve(__dirname, zoroasterEs5)
+
+const SPEC = resolve(__dirname, 'test/spec')
+const SPEC_ES5 = resolve(__dirname, 'es5/test/spec')
+
+const spec = useEs5 ? SPEC_ES5 : SPEC
+const node = useEs5 ? ZOROASTER_ES5 : ZOROASTER
 
 const args = [spec]
 
@@ -14,5 +35,4 @@ if (process.argv.find(argv => argv === '--watch')) {
   args.push('--watch')
 }
 
-fork(zoroaster, args)
-  .on('exit', process.exit)
+fork(node, args)
