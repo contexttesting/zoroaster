1be601d825c108b32fb8bacca1f65324617fcf04
Anton <anton@sobesednik.media>
2018-03-15 11:06:17 +0000

    Update readme

--- examples/test/Zoroaster_test.js
+++ examples/test/Zoroaster_test.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const { assert } = require('../../assert')
+const { resolve } = require('path')
+const { assert, equal } = require('../../assert')
 const Zoroaster = require('../src/Zoroaster')
 
 const Zoroaster_test_suite = {
@@ -9,46 +9,46 @@ const Zoroaster_test_suite = {
   },
 
   // recursive test suites
-  constructor: {
+  standard_constructor: {
     'should create a new Zoroaster instance with default name'() {
       const zoroaster = new Zoroaster()
       assert(zoroaster instanceof Zoroaster)
-      assert(zoroaster.name === 'Zarathustra')
+      equal(zoroaster.name, 'Zarathustra')
     },
     'should create a new Zoroaster instance with a name'() {
       const name = 'Ashu Zarathushtra'
       const zoroaster = new Zoroaster(name)
-      assert(zoroaster.name === name)
+      equal(zoroaster.name, name)
 
       const name2 = 'Zarathushtra Spitama'
       const zoroaster2 = new Zoroaster(name2)
-      assert(zoroaster2.name === name2)
+      equal(zoroaster2.name, name2)
     },
     'should have balance of 0 when initialised'() {
       const zoroaster = new Zoroaster()
-      assert(zoroaster.balance === 0)
+      equal(zoroaster.balance, 0)
     },
   },
 
   methods: {
     // pass a test suite as a path to the file
-    side: path.join(__dirname, 'methods', 'side'),
-    say: path.join(__dirname, 'methods', 'say'),
+    side: resolve(__dirname, 'methods/side'),
+    say: resolve(__dirname, 'methods/say'),
 
     // some more standard test cases
-    createWorld() {
+    'should create a world'() {
       const zoroaster = new Zoroaster()
       zoroaster.createWorld()
-      assert(zoroaster.balance === 100)
+      equal(zoroaster.balance, 100)
     },
-    destroyWorld() {
+    'should destroy a world'() {
       const zoroaster = new Zoroaster()
       zoroaster.createWorld()
       zoroaster.destroyWorld()
-      assert(zoroaster.balance === 0)
+      equal(zoroaster.balance, 0)
     },
     checkParadise: {
-      async 'should return true when balance of 1000 met'() {
+      async 'should return true when balance of 1000 met'() { // wow what syntax
         const zoroaster = new Zoroaster()
         zoroaster.createWorld()
         await Promise.all(
@@ -56,12 +56,13 @@ const Zoroaster_test_suite = {
             await zoroaster.side(Zoroaster.AHURA_MAZDA)
           })
         )
-        assert(zoroaster.balance === 1000)
+        equal(zoroaster.balance, 1000)
         assert(zoroaster.checkParadise())
       },
       'should return false when balance is less than 1000'() {
         const zoroaster = new Zoroaster()
-        assert(zoroaster.checkParadise() === false)
+        const actual = zoroaster.checkParadise()
+        assert(actual === false)
       },
     },
   },
