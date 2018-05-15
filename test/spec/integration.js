8588a822d14e27a66b6bfee87507c4439c5778fe
Anton <anton@adc.sh>
2018-05-15 04:02:06 +0100

    update snapshot context & make integration test pass

--- test/spec/integration.js
+++ test/spec/integration.js
@@ -1,34 +1,38 @@
 import { fork } from 'spawncommand'
-import { resolve, join, basename } from 'path'
+import { resolve } from 'path'
 import stripAnsi from 'strip-ansi'
-import { EOL } from 'os'
-import snapshotContext, { SnapshotContext } from 'snapshot-context'
-import context, { Context} from '../context'
+import snapshotContext, { SnapshotContext } from 'snapshot-context' // eslint-disable-line
+import context, { Context } from '../context' // eslint-disable-line
 
 const SNAPSHOT_DIR = resolve(__dirname, '../snapshot')
 const BIN = resolve(__dirname, '../../src/bin')
 
-const re = new RegExp(process.cwd(), 'g')
+const re = new RegExp(process.cwd().replace(/\\/g, '\\\\'), 'g')
+const winRe = new RegExp(process.cwd().replace(/\\/g, '/'), 'g')
+
 function getSnapshot(s) {
   const snapshot = stripAnsi(s).trim()
-  return snapshot.replace(re, '')
+  return snapshot
+    .replace(re, '')
+    .replace(winRe, '')
+    .replace(/\\/g, '/')
+    .replace(/\r?\n/g, '\n')
 }
-
-// const WIN = /^win/.test(process.platform)
 
 /** @type {Object.<string, (ctx: Context, snapshotCtx: SnapshotContext)>} */
 const t = {
   context:[
     context,
-    snapshotContext
+    snapshotContext,
   ],
   async 'produces correct output'({ TEST_SUITE_PATH }, { test, setDir }) {
     setDir(SNAPSHOT_DIR)
     const { promise } = fork(BIN, [TEST_SUITE_PATH], {
       stdio: 'pipe',
     })
-    const { stdout, stderr } = await promise
-    await test('integration-stdout.txt', getSnapshot(stdout))
+    const { stdout } = await promise
+    const s = getSnapshot(stdout)
+    await test('integration-stdout.txt', s)
   },
 }
 
