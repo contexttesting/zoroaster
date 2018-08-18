aff148ac3bd07982e3d24b63beeb29c317b92b50
Anton <anton@adc.sh>
2018-08-18 23:20:48 +0100

    build

--- build/index.js
+++ build/index.js
@@ -1,6 +1,5 @@
 const { resolve } = require('path')
 const { fork } = require('spawncommand')
-let makeTestSuite = require('./lib/make-test-suite'); if (makeTestSuite && makeTestSuite.__esModule) makeTestSuite = makeTestSuite.default;
 
 const BIN = resolve(__dirname, 'bin', process.env.BABEL_ENV == 'test-build' ? 'zoroaster.js' : 'index.js')
 
@@ -21,7 +20,22 @@ function run(args, options = {}) {
 
 module.exports = run
 
-const $default = require('./lib/mask')
+const $default = require('./lib/make-test-suite')
 
-module.exports.getTests = $default
-module.exports.makeTestSuite = makeTestSuite
+/* documentary types/make-test-suite.xml */
+/**
+ * @typedef {Object} Context A context made with a constructor.
+ * @prop {() => void} [_init] A function to initialise the context.
+ * @prop {() => void} [_destroy] A function to destroy the context.
+ *
+ * @typedef {Object} MakeTestSuiteConf Configuration for making test suites.
+ * @prop {({new(): Context}|{new(): Context}[]|{})} [context] Single or multiple context constructors or objects to initialise for each test.
+ * @prop {(input: string, ...contexts?: Context[]) => string} [getResults] A function which should return results of a test.
+ * @prop {(input: string, ...contexts?: Context[]) => { fn: function, args?: any[], message?: (string|RegExp) }} [getThrowsConfig] A function which should return a configuration for [`assert-throws`](https://github.com/artdecocode/assert-throws), including `fn` and `args`, when testing an error.
+ * @prop {(results: any) => string} [mapActual] An optional function to get a value to test against `expected` mask property from results. By default, the full result is used.
+ * @prop {(results: any, props: Object.<string, (string|object)>) => void} [assertResults] A function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask using `customProps` and `jsonProps` will be passed as first and second arguments.
+ * @prop {string[]} [customProps] An array of custom properties' names to extract from the mask.
+ * @prop {string[]} [jsonProps] Any additional properties to extract from the mask, and parse as _JSON_ values.
+ */
+
+module.exports.makeTestSuite = $default
