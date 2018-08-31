a2f2d34f460807c3ba1cb3b4c8002810fb477c74
Anton <anton@adc.sh>
2018-08-31 22:06:16 +0300

    add focused tests

---
+++ test/spec/TestSuite/focus.js
@@ -0,0 +1,99 @@
+import { deepEqual } from 'assert'
+import TestSuite from '../../../src/lib/TestSuite'
+import Context from '../../context'
+
+/** @type {Object.<string, (c: Context)>} */
+const T = {
+  context: Context,
+  async 'runs focused tests across test suites'({ TEST_SUITE_NAME }) {
+    const run = []
+    const ts = new TestSuite(TEST_SUITE_NAME, {
+      testSuiteA: {
+        'testAA'() {
+          run.push('testAA')
+        },
+        '!testAB'() {
+          run.push('!testAB')
+        },
+        'testAC'() {
+          run.push('testAC')
+        },
+      },
+      testSuiteB: {
+        'testBA'() {
+          run.push('testBA')
+        },
+        '!testBB'() {
+          run.push('!testBB')
+        },
+        'testBC'() {
+          run.push('testBC')
+        },
+      },
+    })
+    await ts.run(undefined, ts.hasFocused)
+    deepEqual(run, ['!testAB', '!testBB'])
+  },
+  async 'runs all tests inside focused test suite'({ TEST_SUITE_NAME }) {
+    const run = []
+    const ts = new TestSuite(TEST_SUITE_NAME, {
+      '!testSuiteA': {
+        'testAA'() {
+          run.push('testAA')
+        },
+        'testAB'() {
+          run.push('testAB')
+        },
+        'testAC'() {
+          run.push('testAC')
+        },
+      },
+      testSuiteB: {
+        'testBA'() {
+          run.push('testBA')
+        },
+        'testBB'() {
+          run.push('testBB')
+        },
+        'testBC'() {
+          run.push('testBC')
+        },
+      },
+    })
+    await ts.run(undefined, ts.hasFocused)
+    deepEqual(run, ['testAA', 'testAB', 'testAC'])
+  },
+  async 'runs all tests inside a focused test suite and other tests'({
+    TEST_SUITE_NAME,
+  }) {
+    const run = []
+    const ts = new TestSuite(TEST_SUITE_NAME, {
+      '!testSuiteA': {
+        'testAA'() {
+          run.push('testAA')
+        },
+        'testAB'() {
+          run.push('testAB')
+        },
+        'testAC'() {
+          run.push('testAC')
+        },
+      },
+      testSuiteB: {
+        'testBA'() {
+          run.push('testBA')
+        },
+        '!testBB'() {
+          run.push('!testBB')
+        },
+        'testBC'() {
+          run.push('testBC')
+        },
+      },
+    })
+    await ts.run(undefined, ts.hasFocused)
+    deepEqual(run, ['testAA', 'testAB', 'testAC', '!testBB'])
+  },
+}
+
+export default T
