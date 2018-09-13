8fe613a37a6eca2bfe160f9ea92ee23a6714f33e
Anton <anton@adc.sh>
2018-09-13 05:32:06 +0300

    build

--- build/lib/make-test-suite.js
+++ build/lib/make-test-suite.js
@@ -1,22 +1,40 @@
 let erte = require('erte'); if (erte && erte.__esModule) erte = erte.default;
+const { readdirSync, lstatSync } = require('fs');
+const { resolve, join } = require('path');
 let getTests = require('../lib/mask'); if (getTests && getTests.__esModule) getTests = getTests.default;
 const { equal, throws } = require('../assert');
 
-// The `expected` property of the mask will be compared against the actual value returned by the `getActual` function. To test for the correct error message, the `error` property will be tested using `assert-throws` configuration returned by `getThrowsConfig` function. Any additional tests can be performed with `customTest` function, which will receive any additional properties extracted from the mask using `customProps` and `jsonProps`. The JSON properties will be parsed into an object.
-
 /**
  * Make a test suite to test against a mask.
- * @param {string} maskPath Path to the mask.
+ * @param {string} path Path to the mask file or directory of files.
  * @param {MakeTestSuiteConf} [conf] Configuration for making test suites.
  * @param {({new(): Context}|{new(): Context}[]|{})} [conf.context] Single or multiple context constructors or objects to initialise for each test.
  * @param {(input: string, ...contexts?: Context[]) => string} [conf.getResults] A function which should return results of a test.
  * @param {(input: string, ...contexts?: Context[]) => { fn: function, args?: any[], message?: (string|RegExp) }} [conf.getThrowsConfig] A function which should return a configuration for [`assert-throws`](https://github.com/artdecocode/assert-throws), including `fn` and `args`, when testing an error.
  * @param {(results: any) => string} [conf.mapActual] An optional function to get a value to test against `expected` mask property from results. By default, the full result is used.
- * @param {(results: any, props: Object.<string, (string|object)>) => void} [conf.assertResults] A function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask using `customProps` and `jsonProps` will be passed as first and second arguments.
- * @param {string[]} [conf.customProps] An array of custom properties' names to extract from the mask.
+ * @param {(results: any, props: Object.<string, (string|object)>) => void} [conf.assertResults] A function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask (where `jsonProps` are parsed into JS objects) will be passed as arguments.
  * @param {string[]} [conf.jsonProps] Any additional properties to extract from the mask, and parse as _JSON_ values.
  */
-const makeTestSuite = (maskPath, conf) => {
+               function makeTestSuite(path, conf) {
+  const pathStat = lstatSync(path)
+  if (pathStat.isFile()) {
+    return makeATestSuite(path, conf)
+  } else if (pathStat.isDirectory()) {
+    const content = readdirSync(path)
+    const res = content.reduce((acc, node) => {
+      const newPath = join(path, node)
+      return {
+        ...acc,
+        [node]: makeTestSuite(newPath, conf),
+      }
+    }, {})
+    return res
+  }
+}
+
+// The `expected` property of the mask will be compared against the actual value returned by the `getActual` function. To test for the correct error message, the `error` property will be tested using `assert-throws` configuration returned by `getThrowsConfig` function. Any additional tests can be performed with `customTest` function, which will receive any additional properties extracted from the mask using `customProps` and `jsonProps`. The JSON properties will be parsed into an object.
+
+const makeATestSuite = (maskPath, conf) => {
   if (!conf) throw new Error('No configuration is given. A config should at least contain either a "getThrowsConfig" or "getResults" functions.')
   const {
     context,
@@ -24,15 +42,9 @@ const makeTestSuite = (maskPath, conf) => {
     getThrowsConfig,
     mapActual = a => a,
     assertResults,
-    customProps = [],
     jsonProps = [],
   } = conf
-  const tests = getTests(maskPath, [
-    'expected',
-    'error',
-    ...customProps,
-    ...jsonProps,
-  ])
+  const tests = getTests(maskPath)
 
   const hasFocused = tests.some(({ name }) => name.startsWith('!'))
 
@@ -119,10 +131,11 @@ const assertExpected = (result, expected) => {
  * @prop {(input: string, ...contexts?: Context[]) => string} [getResults] A function which should return results of a test.
  * @prop {(input: string, ...contexts?: Context[]) => { fn: function, args?: any[], message?: (string|RegExp) }} [getThrowsConfig] A function which should return a configuration for [`assert-throws`](https://github.com/artdecocode/assert-throws), including `fn` and `args`, when testing an error.
  * @prop {(results: any) => string} [mapActual] An optional function to get a value to test against `expected` mask property from results. By default, the full result is used.
- * @prop {(results: any, props: Object.<string, (string|object)>) => void} [assertResults] A function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask using `customProps` and `jsonProps` will be passed as first and second arguments.
- * @prop {string[]} [customProps] An array of custom properties' names to extract from the mask.
+ * @prop {(results: any, props: Object.<string, (string|object)>) => void} [assertResults] A function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask (where `jsonProps` are parsed into JS objects) will be passed as arguments.
  * @prop {string[]} [jsonProps] Any additional properties to extract from the mask, and parse as _JSON_ values.
  */
 
-module.exports=makeTestSuite
+// export default makeTestSuite
+
+module.exports = makeTestSuite
 //# sourceMappingURL=make-test-suite.js.map
