e595e6b1603571025906a9e7071900f46f263230
Anton <anton@adc.sh>
2019-03-16 15:22:31 +0300

    debug

--- src/lib/run-test.js
+++ src/lib/run-test.js
@@ -2,46 +2,8 @@ import { EOL } from 'os'
 import reducer, { runTest } from '@zoroaster/reducer'
 import { evaluateContext, destroyContexts } from '@zoroaster/reducer/build/lib'
 import promto from 'promto'
-import { join } from 'path'
-import SnapshotContext from 'snapshot-context'
-import exists from '@wrote/exists'
-import { c } from 'erte'
 import { TICK, CROSS, indent, filterStack, replaceFilename } from '.'
-
-const handleSnapshot = async (result, name, path, snapshotDir, snapshotRoot) => {
-  const nn = name.replace(/^!/, '')
-  const n = nn.replace(/ /g, '-')
-  const ext = typeof result == 'string' ? 'txt' : 'json'
-  const snapshotFilename = `${n}.${ext}`
-  let pp = join(...path)
-  const root = snapshotRoot.find(r => pp.startsWith(r))
-  if (root) pp = pp.slice(root.length)
-  let p = join(snapshotDir, pp)
-
-  if (result) {
-    const sc = new SnapshotContext()
-    sc.setDir(p)
-    const otherSnapshot = snapshotFilename
-      .replace(/(json|txt)$/, (m) => {
-        if (m == 'txt') return 'json'
-        return 'txt'
-      })
-    const e = await exists(join(p, otherSnapshot))
-    if (e)
-      throw new Error(`Snapshot of another type exists: ${otherSnapshot}`)
-    await sc.test(snapshotFilename, result, c(nn, 'yellow'))
-  } else {
-    let snapshotPath = join(p, snapshotFilename)
-    let e = await exists(snapshotPath)
-    if (!e) {
-      snapshotPath = snapshotPath.replace(/json$/, 'txt')
-      e = await exists(snapshotPath)
-    }
-    if (e) {
-      throw new Error(`Snapshot ${snapshotPath} exists, but the test did not return anything.`)
-    }
-  }
-}
+import handleSnapshot from './snapshot'
 
 /**
  * Run the test.
---
+++ src/lib/snapshot.js
@@ -0,0 +1,42 @@
+import { join } from 'path'
+import SnapshotContext from 'snapshot-context'
+import exists from '@wrote/exists'
+import { c } from 'erte'
+
+const handleSnapshot = async (result, name, path, snapshotDir, snapshotRoot) => {
+  const nn = name.replace(/^!/, '')
+  const n = nn.replace(/ /g, '-')
+  const ext = typeof result == 'string' ? 'txt' : 'json'
+  const snapshotFilename = `${n}.${ext}`
+  let pp = join(...path)
+  const root = snapshotRoot.find(r => pp.startsWith(r))
+  if (root) pp = pp.slice(root.length)
+  let p = join(snapshotDir, pp)
+
+  if (result) {
+    const sc = new SnapshotContext()
+    sc.setDir(p)
+    const otherSnapshot = snapshotFilename
+      .replace(/(json|txt)$/, (m) => {
+        if (m == 'txt') return 'json'
+        return 'txt'
+      })
+    const e = await exists(join(p, otherSnapshot))
+    if (e)
+      throw new Error(`Snapshot of another type exists: ${otherSnapshot}`)
+    console.log(p, snapshotFilename)
+    await sc.test(snapshotFilename, result, c(nn, 'yellow'))
+  } else {
+    let snapshotPath = join(p, snapshotFilename)
+    let e = await exists(snapshotPath)
+    if (!e) {
+      snapshotPath = snapshotPath.replace(/json$/, 'txt')
+      e = await exists(snapshotPath)
+    }
+    if (e) {
+      throw new Error(`Snapshot ${snapshotPath} exists, but the test did not return anything.`)
+    }
+  }
+}
+
+export default handleSnapshot
