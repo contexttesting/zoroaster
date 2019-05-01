6e8eb3339646df20baee4d458e6aa11d15f8a647
Anton <anton@adc.sh>
2019-05-01 19:06:58 +0300

    rm build

--- build/assert/index.js
+++
@@ -1,9 +0,0 @@
-const $assert_throws = require('assert-throws');
-const $assert = require('assert');
-const $zoroaster_deep_equal = require('@zoroaster/deep-equal');
-
-module.exports.throws = $assert_throws
-module.exports.assert = $assert
-module.exports.equal = $assert.equal
-module.exports.ok = $assert.ok
-module.exports.deepEqual = $zoroaster_deep_equal
